a2f6e03aaf8e40b5dc3f7f40fd25e2d9a3edafd8
Arghadeep Dey <deyarghadeep23@gmail.com>
2023-11-30 17:33:12 +0530

    changed schema and added css

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ const hbs = require("hbs")
 const {connectdb,collection} = require("./mongodb")
 
 const templatePath = path.join(__dirname,'../templates')
-
+const publicPath = path.join(__dirname, '../public');
 app.use(express.json())
 app.set("view engine","hbs")
 app.set("views",templatePath)
 app.use(express.urlencoded({extended:false}))
+app.use(express.static(publicPath));
 connectdb()
 
 app.get("/",(req,res)=>{
@@ -19,11 +20,19 @@ app.get("/",(req,res)=>{
 app.get("/signup",(req,res)=>{
     res.render("signup")
 })
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+app.get("/home",(req,res)=>{
+    res.render("home")
+})
 
 app.post("/signup", async(req,res)=>{
     
     const data = {
-        name:req.body.name,
+        firstName:req.body.first_name,
+        lastName:req.body.second_name,
+        email:req.body.email,
         password:req.body.password
     }
 

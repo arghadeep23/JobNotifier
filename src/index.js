f0ac42f72601d13d5673192e867bcfef70c47a33
sethiyadivyanshu30 <sethiyadivyanshu1@gmail.com>
2023-11-30 16:37:48 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -36,16 +36,13 @@ app.post('/login', async (req, res) => {
 
     try {
         const check = await collection.findOne({ name: req.body.name })
-
         if (check.password === req.body.password) {
             res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` })
         }
-
         else {
             res.send("incorrect password")
         }
     } 
-
     catch (e) {
         res.send("wrong details")
     }
